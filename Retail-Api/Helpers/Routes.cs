43ef8342f0c008199a41a516b49c99bad59f5246
Veljko28 <charlibear28@gmail.com>
2021-05-01 10:11:51 +0200

    Adds Product Routes
    Creates a class called routes that is called in controllers, and creates a products class that has the routes of all the product uri

---
+++ Retail-Api/Helpers/Routes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Retail_Api.Helpers
+{
+	public static class Routes
+	{
+		public const string Version = "v1";
+
+		public static class Products
+		{
+			public const string RouteType = "/product";
+
+			public const string GetAll = Version + "/products";
+
+			public const string GetById = Version + RouteType + "/{productId}";
+			
+			public const string Add = Version + RouteType + "/add";
+
+			public const string DeleteById = Version + RouteType + "/{productId}";
+
+			public const string UpdateById = Version + RouteType + "/{productId}";
+
+		}
+	}
+}
