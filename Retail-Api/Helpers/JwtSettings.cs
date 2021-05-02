609c179002038a759d597250f74ed4e7943295e5
Veljko28 <charlibear28@gmail.com>
2021-05-02 10:28:26 +0200

    Adds JWT Tokes
    Adds the JWT Settings and JWT Installer for the startup class

---
+++ Retail-Api/Helpers/JwtSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Retail_Api.Helpers
+{
+	public class JwtSettings
+	{
+		public string Secret { get; set; }
+	}
+}
---
+++ Retail-Api/Installers/JwtInstaller.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Retail_Api.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Retail_Api.Installers
+{
+	public class JwtInstaller : IInstaller
+	{
+		public void InstallServices(IConfiguration configuration, IServiceCollection services)
+		{
+			var JwtSettings = new JwtSettings();
+			configuration.Bind(nameof(JwtSettings), JwtSettings);
+
+			services.AddSingleton(JwtSettings);
+
+			services.AddAuthentication(x =>
+			{
+				x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+				x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+			}).AddJwtBearer(x => {
+				x.SaveToken = true;
+				x.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JwtSettings.Secret)),
+					ValidateIssuer = false,
+					ValidateAudience = false,
+					RequireExpirationTime = false,
+					ValidateLifetime = true
+				};
+
+			});
+		}
+	}
+}
