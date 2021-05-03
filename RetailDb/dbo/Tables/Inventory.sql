0f8eedf2013338b2615443b0f579252956655a26
Veljko28 <charlibear28@gmail.com>
2021-05-03 19:15:11 +0200

    SQL Tables
    Adds the updated sql tables with the user password field

---
+++ RetailDb/dbo/Tables/Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
