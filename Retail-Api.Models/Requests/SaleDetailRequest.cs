77e11b6f1c0d51534efe03e075ec284cb0655211
Veljko28 <charlibear28@gmail.com>
2021-05-07 13:55:03 +0200

    SaleDetail Model
    Adds the SaleDetail Request model to the models project

---
+++ Retail-Api.Models/Requests/SaleDetailRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Retail_Api.Models.Requests
+{
+	public class SaleDetailRequest
+	{
+
+		[Required]
+		public int SaleId { get; set; }
+
+		[Required]
+		public int ProductId { get; set; }
+
+		[Required]
+		public int Quantity { get; set; } = 1;
+
+		[Required]
+		public decimal PurchasePrice { get; set; }
+
+		[Required]
+		public decimal Tax { get; set; } = 0;
+	}
+}
