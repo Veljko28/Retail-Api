d5e80f10adbcf7bf72b564c89a49e896ab1ddcc9
Veljko28 <charlibear28@gmail.com>
2021-05-08 08:38:57 +0200

    Returning Token
    Class for the token that is returned in the account controller. Returns the token value and its expiry date.

---
+++ Retail-Api/Helpers/TokenResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Retail_Api.Helpers
+{
+	public class TokenResponse
+	{
+		public string Token { get; set; }
+
+		public string Expires { get; set; }
+	}
+}
