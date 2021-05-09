ab8dc18487e0501401cea58fd98b56e9fd410a5d
Veljko28 <charlibear28@gmail.com>
2021-05-09 08:02:16 +0200

    Inventory Request
    Adds the Inventory Request model to the model class library

---
+++ Retail-Api.Models/Requests/InventoryRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Retail_Api.Models.Requests
+{
+	public class InventoryRequest
+	{
+		[Required]
+		public int ProductId { get; set; }
+
+		[Required]
+		public int Quantity { get; set; } = 1;
+
+		[Required]
+		public decimal PurchasePrice { get; set; }
+
+		[Required]
+		public DateTime PurchaseDate { get; set; } = DateTime.UtcNow;
+	}
+}
