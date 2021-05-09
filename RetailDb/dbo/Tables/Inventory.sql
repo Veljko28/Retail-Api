52e0536d01a3ea7406dded27c8bb9876f322cac3
Veljko28 <charlibear28@gmail.com>
2021-05-09 08:04:00 +0200

    Table Update
    Adds the Identity to the Inventory id field in the sql table

--- RetailDb/dbo/Tables/Inventory.sql
+++ RetailDb/dbo/Tables/Inventory.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Inventory]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [ProductId] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1, 
     [PurchasePrice] MONEY NOT NULL, 
