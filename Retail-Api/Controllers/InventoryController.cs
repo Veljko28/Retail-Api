50fdd2f99c4eb7c40cde99a65a30a3d4908d8f00
Veljko28 <charlibear28@gmail.com>
2021-05-10 06:39:39 +0200

    Controller For the Inventory
    Creates the controller for the inventory with its end points with authentication

---
+++ Retail-Api/Controllers/InventoryController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Retail_Api.Helpers;
+using Retail_Api.Models;
+using Retail_Api.Models.Requests;
+using Retail_Api.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Retail_Api.Controllers
+{
+	[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public class InventoryController : Controller
+	{
+		private readonly IInventoryRepository _inventory;
+
+		public InventoryController(IInventoryRepository inventory)
+		{
+			_inventory = inventory;
+		}
+
+		[HttpGet(Routes.InventoryRoutes.All)]
+		public async Task<IActionResult> All()
+		{
+			IEnumerable<Inventory> inv =  await _inventory.getAllInInventoryAsync();
+
+			if (inv.FirstOrDefault() == null)
+			{
+				return NotFound("Cannot find any inventory");
+			}
+			return Ok(inv);
+		}
+
+
+		[HttpPost(Routes.InventoryRoutes.Add)]
+		public async Task<IActionResult> Add([FromBody] InventoryRequest request)
+		{
+			Inventory inv = await _inventory.addToInventoryAsync(request);
+
+			if (inv == null)
+			{
+				return BadRequest("Cannot add this to the inventory");
+			}
+			return Ok(inv);
+		}
+
+
+		[HttpGet(Routes.InventoryRoutes.GetById)]
+		public async Task<IActionResult> GetById([FromRoute] int invId)
+		{
+			Inventory inv = await _inventory.getByIdAsync(invId);
+
+			if (inv == null)
+			{
+				return NotFound("Cannot find this item in the inventory");
+			}
+			return Ok(inv);
+		}
+	}
+}
