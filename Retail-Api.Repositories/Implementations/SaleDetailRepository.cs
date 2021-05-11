2c48691612f039caeca0ccfbfbe904c813751dd3
Veljko28 <charlibear28@gmail.com>
2021-05-11 07:45:32 +0200

    Adds Details
    Adding sale detail repository interface and its implementation.

---
+++ Retail-Api.Repositories/Implementations/SaleDetailRepository.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Retail_Api.Models;
+using Retail_Api.Models.Requests;
+using Retail_Api.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Retail_Api.Repositories.Implementations
+{
+	public class SaleDetailRepository : ISaleDetailRepository
+	{
+		private readonly IConfiguration _configuration;
+		public SaleDetailRepository(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public async Task<SaleDetail> createAsync(SaleDetailRequest saleRequest)
+		{
+			string sql = "exec [AddSaleDetail] @SaleId, @ProductId, @Quantity, @PurchasePrice, @Tax";
+
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+				int rowsModified = await db.ExecuteAsync(sql, saleRequest);
+
+				string sale = "SELECT * FROM SaleDetail WHERE Id=(SELECT max(Id) FROM SaleDetail)";
+
+				var sales = await db.QueryAsync<SaleDetail>(sale);
+				return sales.FirstOrDefault();
+			}
+		}
+
+		public async Task<IEnumerable<SaleDetail>> getAllAsync()
+		{
+			string sql = "exec [GetSaleDetails]";
+
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				var saleDetails = await db.QueryAsync<SaleDetail>(sql);
+				return saleDetails;
+			}
+		}
+
+		public async Task<SaleDetail> getByIdAsync(int id)
+		{
+			string sql = "exec [GetSaleDetailById] @Id";
+
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				SaleDetail result = (await db.QueryAsync<SaleDetail>(sql, new { Id = id })).FirstOrDefault();
+
+				return result;
+			}
+
+		}
+	
+	}
+}
---
+++ Retail-Api.Repositories/Interfaces/ISaleDetailRepository.cs
@@ -0,0 +1,19 @@
+﻿using Retail_Api.Models;
+using Retail_Api.Models.Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail_Api.Repositories.Interfaces
+{
+	public interface ISaleDetailRepository
+	{
+		Task<SaleDetail> createAsync(SaleDetailRequest saleRequest);
+
+		Task<IEnumerable<SaleDetail>> getAllAsync();
+
+		Task<SaleDetail> getByIdAsync(int id);
+
+	}
+}
