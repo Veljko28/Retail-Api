deb4d3d3a571a0fabadb51b46b17bbfe3b0bf720
Veljko28 <charlibear28@gmail.com>
2021-05-12 13:38:12 +0200

    Refresh Token Models
    Adding the Refresh token model and its model request

---
+++ Retail-Api.Models/RefreshToken.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Retail_Api.Models
+{
+	public class RefreshToken
+	{
+		[Key]
+		public string Token { get; set; }
+		public string JwtId { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public DateTime Expires { get; set; }
+		public bool Used { get; set; }
+		public bool  Invalidated { get; set; }
+		public string UserId { get; set; }
+
+		[ForeignKey(nameof(UserId))]
+		public User user { get; set; }
+	}
+}
---
+++ Retail-Api.Models/Requests/RefreshTokenRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Retail_Api.Models.Requests
+{
+	public class RefreshTokenRequest
+	{
+		public string Token { get; set; }
+
+		public string RefreshToken { get; set; }
+	}
+}
