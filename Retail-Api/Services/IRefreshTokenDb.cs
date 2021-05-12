ce53de1cddd011b889da485be7f27083a6c6755a
Veljko28 <charlibear28@gmail.com>
2021-05-12 13:36:21 +0200

    Refresh Tokens Db
    Adds the refresh token database access interface and its implementation.

---
+++ Retail-Api/Services/IRefreshTokenDb.cs
@@ -0,0 +1,18 @@
+﻿using Retail_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Retail_Api.Services
+{
+	public interface IRefreshTokenDb
+	{
+		Task<RefreshToken> findTokenAsync(string token);
+
+		Task<bool> addTokenAsync(RefreshToken refreshToken);
+
+		Task<bool> updateTokenAsync(RefreshToken refreshToken);
+
+	}
+}
---
+++ Retail-Api/Services/RefreshTokenDb.cs
@@ -0,0 +1,63 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Retail_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Retail_Api.Services
+{
+	public class RefreshTokenDb : IRefreshTokenDb
+	{
+		private readonly IConfiguration _configuration;
+
+		public RefreshTokenDb(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public async Task<bool> addTokenAsync(RefreshToken refreshToken)
+		{
+			string sql = "exec [AddRefreshToken] @Token, @JwtId, @CreatedDate, @Expires, @Used, @Invalidated, @UserId";
+
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				int rowsModified = await db.ExecuteAsync(sql, refreshToken);
+
+				return rowsModified > 0;
+			}
+		}
+
+		public async Task<RefreshToken> findTokenAsync(string token)
+		{
+			string sql = "exec [FindRefreshToken] @Token";
+
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				var response = await db.QueryAsync<RefreshToken>(sql, new { Token = token });
+
+				return response.FirstOrDefault();
+			}
+		}
+
+		public async Task<bool> updateTokenAsync(RefreshToken refreshToken)
+		{
+			string sql = "exec [UpdateRefeshToken] @Token, @JwtId, @CreatedDate, @Expires, @Used, @Invalidated, @UserId";
+
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				int rowsModified = await db.ExecuteAsync(sql,refreshToken);
+
+				return rowsModified > 0;
+			}
+		}
+	}
+}
