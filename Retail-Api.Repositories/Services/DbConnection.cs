5bd14eee510d8fcdc88d373397ad5498e08941fb
Veljko28 <charlibear28@gmail.com>
2021-05-15 11:47:57 +0200

    Generic Repository Actions
    Adds the generic repository actions (getbyid and get all)

---
+++ Retail-Api.Repositories/Services/DbConnection.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail_Api.Models.Services
+{
+	public class DbConnection
+	{
+		public static async Task<T> GenericConnectionID<T>(int Id, string sql, IConfiguration _configuration)
+		{
+
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+
+				T result = (await db.QueryAsync<T>(sql, new { Id })).FirstOrDefault();
+
+				return result;
+			}
+
+		}
+
+
+		public static async Task<IEnumerable<T>> GenericConnectionAll<T>(string sql, IConfiguration _configuration)
+		{
+			using (SqlConnection db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				await db.OpenAsync();
+				IEnumerable<T> allOfType = await db.QueryAsync<T>(sql);
+				return allOfType;
+			}
+		}
+	}
+}
