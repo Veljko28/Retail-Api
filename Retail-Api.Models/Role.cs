4f62bd8829039eaa0e543a8f619c3e462f51fee5
Veljko28 <charlibear28@gmail.com>
2021-05-16 09:45:59 +0200

    Adding Role Model
    Model for the user role that contains userid and roleid

---
+++ Retail-Api.Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Retail_Api.Models
+{
+	public class Role
+	{
+		public string UserId { get; set; }
+		public string RoleId { get; set; }
+
+	}
+}
