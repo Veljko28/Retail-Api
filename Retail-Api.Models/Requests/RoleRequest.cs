bc32622d3109c66803adf42e7a3ae4a8eaa725d7
Veljko28 <charlibear28@gmail.com>
2021-05-19 18:47:49 +0200

    Adding Role Request model
    Role Request model for the controller requests

---
+++ Retail-Api.Models/Requests/RoleRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Retail_Api.Models.Requests
+{
+	public class RoleRequest
+	{
+		public string UserId { get; set; }
+
+		public string RoleId { get; set; }
+	}
+}
