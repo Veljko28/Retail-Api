cf5b6bc01d01b07f07748fac750cb506e783daaf
Veljko28 <charlibear28@gmail.com>
2021-05-21 14:29:33 +0200

    New Responses
    Generic Response For Api Pagination

---
+++ Retail-Api/Helpers/Responses/Response.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Retail_Api.Helpers.Responses
+{
+	public class Response<T>
+	{
+		public T Data { get; set; }
+		public Response()
+		{
+
+		}
+		public Response(T response)
+		{
+			Data = response;
+		}
+	}
+}
